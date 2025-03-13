3ca2029864540d020ee6826c35ec04d5832b20fa
Ruabbit <liuzhao6526@gmail.com>
2025-03-13 14:55:07 +0800

    fix: ignore passwd

--- models/user.go
+++ models/user.go
@@ -4,8 +4,8 @@ package models
 type User struct {
 	ID       int    `json:"id" gorm:"primaryKey"` // 主键ID
 	Username string `json:"username"`
-	Password string `json:"password"` // 正式环境下请使用加密存储
-	Role     string `json:"role"`     // 例如 "admin" 或 "keeper"
-	Name     string `json:"name"`     // 个人姓名
-	Phone    string `json:"phone"`    // 手机号
+	Password string `json:"-" gorm:"column:password"` // 使用json:"-"来在JSON序列化时忽略该字段
+	Role     string `json:"role"`                     // 例如 "admin" 或 "keeper"
+	Name     string `json:"name"`                     // 个人姓名
+	Phone    string `json:"phone"`                    // 手机号
 }
